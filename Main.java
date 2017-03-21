8c07de492f4eb6728189e9bbbcd7d2eeb805d20f
Bill Mullen <wlmullen@hawaii.edu>
2017-03-20 21:54:05 -1000

    moved driver to Main

--- Main.java
+++ Main.java
@@ -2,17 +2,22 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-/*
 	public static void main(String[] args) {
-//		radio station = new radio();
-		OBS pilot = new OBS();
+		boolean good = true;
+		int radial = 93;
+		int OBSInput = 90;
+		int signalInput = -400;
+		String outputStr = "";
 		
-		station.setRadio();
-		pilot.setHeading();
+		radio radio1 = new radio(radial, good);
 		
-		VORSystem vor = new VORSystem(pilot, station.getRadio());
-		vor.output();
+		signalInput = radio1.produceSignal();
+		
+		VORSystem testSystem = new VORSystem(OBSInput, signalInput);
+		
+		outputStr = testSystem.output();
+				
+		System.out.println("foo");
 	}
-	*/
 }
 
